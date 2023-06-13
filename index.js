8b7436d6bfe29aa458ef92c3445a662eb6877123
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-06-13 22:06:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,75 +1,97 @@
 const searchButton = document.getElementById('searchBtn');
-    const queryInput = document.getElementById('queryInput');
-    const pageNum= document.getElementById('num-pages');
-    const resultsContainer = document.getElementById('results');
+const queryInput = document.getElementById('queryInput');
+const pageNum = document.getElementById('num-pages');
+pageNum.addEventListener('input', function() {
+  this.value = this.value.replace(/\D/g, '');
+});
 
-    
-    searchButton.addEventListener('click', searchJobs);
-    
+const resultsContainer = document.getElementById('results');
 
-    function searchJobs(e) {
-      searchButton.setAttribute('disabled', '');
-      queryInput.value === ''? alert("Please fill out the field") : e.preventDefault();
-      const query = queryInput.value; 
-      const apiKey = 'f46403cfb7mshad35f9146e89e1ep113d1cjsndb129cd6a619';
-      const apiUrl = 'https://jsearch.p.rapidapi.com/search';
-      const page = '1';
-      const numPages = pageNum.value <= 0? 1 : pageNum.value;
-    
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': apiKey,
-          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-        }
-      };  
+searchButton.addEventListener('click', searchJobs);
 
-      fetch(`${apiUrl}?query=${encodeURIComponent(query)}&page=${page}&num_pages=${numPages}`, options)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Request failed');
-          }
-        })
-        .then(result => {
-          const jobDataList = result.data;
-          displayJobInformation(jobDataList);
-          searchButton.removeAttribute('disabled');
-          
-        })
-        .catch(error => {
-          console.error(error);
-          searchButton.removeAttribute('disabled');
-        });
+function searchJobs(e) {
+  searchButton.setAttribute('disabled', '');
+  queryInput.value === '' ? alert("Please fill out the field") : e.preventDefault();
+  const query = queryInput.value;
+  const apiKey = '759e0c73camsh6768755c61b559bp137359jsn1451d9501b34';
+  const apiUrl = 'https://jsearch.p.rapidapi.com/search';
+  const page = '1';
+  const numPages = pageNum.value <= 0 ? 1 : pageNum.value;
+  
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    }
+  };
+
+  fetch(`${apiUrl}?query=${encodeURIComponent(query)}&page=${page}&num_pages=${numPages}`, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Request failed');
+      }
+    })
+    .then(result => {
+      const jobDataList = result.data;
+      displayJobInformation(jobDataList);
+      searchButton.removeAttribute('disabled');
+      queryInput.value= "";
+      pageNum.value= "";
+    })
+    .catch(error => {
+      console.error(error);
+      searchButton.removeAttribute('disabled');
+      queryInput.value= "";
+      pageNum.value= "";
+    });
+}
+
+function displayJobInformation(jobDataList) {
+  if (jobDataList.length === 0) {
+    resultsContainer.innerHTML = "<p style= 'text-align: center;'>No results found.</p>";
+    return;
+  }
+  
+  jobDataList.sort((a, b) => {
+    const dateA = new Date(a.job_posted_at_datetime_utc);
+    const dateB = new Date(b.job_posted_at_datetime_utc);
+    return dateB - dateA;
+  });
+
+  let jobHTML = '';
+
+  jobDataList.forEach(jobData => {
+
+    const jobTitle = jobData.job_title;
+    const employerName = jobData.employer_name;
+    const employerLogo = jobData.employer_logo;
+    const jobDescription = jobData.job_description;
+    const jobApplyLink = jobData.job_apply_link;
+    const jobPostedAtUtc = jobData.job_posted_at_datetime_utc;
+
+    let logoHTML = '';
+    if (employerLogo) {
+      logoHTML = `<img src="${employerLogo}" alt="Employer Logo">`;
     }
 
-    function displayJobInformation(jobDataList) {
-      let jobHTML = '';
+    const datePosted = new Date(jobPostedAtUtc);
+    const formattedDatePosted = datePosted.toLocaleDateString();
 
-      jobDataList.forEach(jobData => {
-        const jobTitle = jobData.job_title;
-        const employerName = jobData.employer_name;
-        const employerLogo = jobData.employer_logo;
-        const jobDescription = jobData.job_description;
-        const jobApplyLink = jobData.job_apply_link;
+    jobHTML += `
+      <div class="card">
+        <h2>${jobTitle}</h2>
+        <p><strong>Employer:</strong> ${employerName}</p>
+        <p><strong>Date Posted:</strong> ${formattedDatePosted}</p>
+        ${logoHTML}
+        <pre>${jobDescription}</pre>
+        <a href="${jobApplyLink}" target="_blank">Apply Now</a>
+      </div>
+    `;
+  });
 
-        let logoHTML = '';
-        if (employerLogo) {
-          logoHTML = `<img src="${employerLogo}" alt="Employer Logo">`;
-        }
-
-        jobHTML += `
-          <div class="card">
-            <h2>${jobTitle}</h2>
-            <p><strong>Employer:</strong> ${employerName}</p>
-            ${logoHTML}
-            <pre>${jobDescription}</pre>
-            <a href="${jobApplyLink}" target="_blank">Apply Now</a>
-          </div>
-        `;
-      });
-      
-      resultsContainer.innerHTML = jobHTML;
-      
-    }
+  resultsContainer.innerHTML = jobHTML;
+}
--- toggle.js
+++ toggle.js
@@ -1,6 +1,32 @@
-const toggler= document.querySelector('.toggle');
-const menu= document.querySelector('.menu');
+//hamburger
+const hamburger = document.querySelector(".hamburger");
+	 const navbar = document.querySelector(".menu");
+	 const navLinks = document.querySelectorAll(".menu a");
+	 
 
-toggler.addEventListener('click', ()=>{
+	 
+const navSlide = () => {
+	 
+     hamburger.onclick = () => {
+		
+	 navbar.classList.toggle("nav-active");
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	  //hamburger animation
+	 hamburger.classList.toggle("toggle");
+    }
+    
+    
+	}
 
-})
+window.onload = () => navSlide();
+
+
+	 
